0de8798d1c2fd9ee6c43e45e6f8966813e15d4d8
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-02 00:39:59 +0700

    DungNguyen

---
+++ Bai2_Trang56/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Bai2_Trang56
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTinhTong = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(121, 79);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(515, 22);
+            this.txtA.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.txtA, "Nhập số tự nhiên A");
+            this.txtA.Enter += new System.EventHandler(this.txtA_Enter);
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_KeyPress);
+            this.txtA.Leave += new System.EventHandler(this.txtA_Leave);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(121, 127);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(515, 22);
+            this.txtB.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.txtB, "Nhập số tự nhiên B");
+            this.txtB.Enter += new System.EventHandler(this.txtA_Enter);
+            this.txtB.Leave += new System.EventHandler(this.txtA_Leave);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nhập A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nhập B:";
+            // 
+            // btnTinhTong
+            // 
+            this.btnTinhTong.Location = new System.Drawing.Point(17, 186);
+            this.btnTinhTong.Name = "btnTinhTong";
+            this.btnTinhTong.Size = new System.Drawing.Size(147, 40);
+            this.btnTinhTong.TabIndex = 2;
+            this.btnTinhTong.Text = "&Tính Tổng";
+            this.btnTinhTong.UseVisualStyleBackColor = true;
+            this.btnTinhTong.Click += new System.EventHandler(this.btnTinhTong_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(248, 186);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(147, 40);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(489, 186);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(147, 40);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua.Location = new System.Drawing.Point(12, 260);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(0, 25);
+            this.lblKetQua.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(655, 359);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnTinhTong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chương trình tính tổng";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTinhTong;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblKetQua;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Bai2_Trang56/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2_Trang56
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtA_Enter(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            txt.BackColor = Color.Turquoise;
+        }
+
+        private void txtA_Leave(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            txt.BackColor = Color.White;
+        }
+
+        private void txtA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Xác thực rằng phím vừa nhấn không phải CTRL hoặc không phải dạng số
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnTinhTong_Click(object sender, EventArgs e)
+        {
+            int soBe, soLon;
+            if(txtA.Text == "")
+            {
+                MessageBox.Show("Cần phải nhập dữ liệu");
+                txtA.Focus();
+                return;
+            }
+            if (txtB.Text == "")
+            {
+                MessageBox.Show("Cần phải nhập dữ liệu");
+                txtB.Focus();
+                return;
+            }
+            if(int.Parse(txtA.Text) == int.Parse(txtB.Text))
+            {
+                lblKetQua.Text = "Không có tổng";
+                return;
+            }    
+            
+            soBe = int.Parse(txtA.Text) < int.Parse(txtB.Text) ? int.Parse(txtA.Text) : int.Parse(txtB.Text);
+            soLon = int.Parse(txtA.Text) + int.Parse(txtB.Text) - soBe;
+            double tong = 0;
+            for(int i = soBe; i <= soLon; i++)
+            {
+                tong += i;
+            }   
+            lblKetQua.Text = "Tổng các số từ " + soBe + " đến " + soLon + " là: " + tong.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblKetQua.Text = "";
+            txtA.Text = "";
+            txtB.Text = "";
+            txtA.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                // user clicked yes
+                this.Close();
+            }
+        }
+
+        
+    }
+}
